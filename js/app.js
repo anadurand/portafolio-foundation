b2778b9173ecad705c0d92b1bf6bb990ab2d6c7c
Mishmore <michellemore28@gmail.com>
2017-05-21 16:29:54 -0500

    black

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
+/*
+if (document.getElementsByClassName("off-canvas")[0].getAttribute("aria-hidden")) {
+}
+if (document.getElementById("buy").getAttribute("aria-expanded")) {
+  document.getElementsByClassName("black-all")[0].classList.add("visible");
+}
+*/
+document.getElementById("buy").addEventListener("click", function(e) {
+  document.getElementsByClassName("black-all")[0].classList.toggle("visible");
+});
+
 $(document).foundation()
 
 //show black modal
+/*
 if (document.getElementsByClassName("is-open")) {
     document.getElementsByClassName("black-all")[0].classList.add("visible");
 }
+else {
+  document.getElementsByClassName("black-all")[0].classList.remove("visible");
+}
+*/
