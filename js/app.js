effcda4a6ddfe5636a7d5a08362c60e9ec1b9d9b
Mishmore <michellemore28@gmail.com>
2017-05-21 16:49:24 -0500

    black modal

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+$(document).foundation()
 /*
 if (document.getElementsByClassName("off-canvas")[0].getAttribute("aria-hidden")) {
 }
@@ -5,12 +6,12 @@ if (document.getElementById("buy").getAttribute("aria-expanded")) {
   document.getElementsByClassName("black-all")[0].classList.add("visible");
 }
 */
+/*
 document.getElementById("buy").addEventListener("click", function(e) {
   document.getElementsByClassName("black-all")[0].classList.toggle("visible");
 });
 
-$(document).foundation()
-
+*/
 //show black modal
 /*
 if (document.getElementsByClassName("is-open")) {
