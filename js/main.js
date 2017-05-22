97d34648500a121af378f3da2346b7b8f185e320
Mishmore <michellemore28@gmail.com>
2017-05-22 12:45:24 -0500

    z index ok

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-
-/*document.getElementById("open-acordeon").addEventListener("click", function() {
-  alert("sfda");
+document.getElementsByClassName("open-acordeon")[0].addEventListener("click", function() {
+  document.getElementsByClassName("acordeon")[0].classList.add("bottom-0");
 })
-alert("asda");
-*/
+
+document.getElementsByClassName("open-acordeon")[1].addEventListener("click", function() {
+  document.getElementsByClassName("acordeon")[0].classList.remove("bottom-0");
+})
+/*
 document.getElementsByClassName("no-bullet")[0].addEventListener("click", function() {
   document.getElementsByClassName("acordeon")[0].classList.toggle("bottom-0");
 })
+*/
