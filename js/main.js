cba8f6d11ddfd2af156ab7a8354e77298e91c22f
Mishmore <michellemore28@gmail.com>
2017-05-22 12:25:54 -0500

    open

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+/*document.getElementById("open-acordeon").addEventListener("click", function() {
+  alert("sfda");
+})
+alert("asda");
+*/
+document.getElementsByClassName("no-bullet")[0].addEventListener("click", function() {
+  document.getElementsByClassName("acordeon")[0].classList.toggle("bottom-0");
+})
