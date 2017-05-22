82e790bc755ee2214abcef52bbabe30d431a8bcc
Mishmore <michellemore28@gmail.com>
2017-05-22 14:33:45 -0500

    price

--- js/main.js
+++ js/main.js
@@ -13,3 +13,8 @@ document.getElementsByClassName("checkout")[0].addEventListener("click", functio
 document.getElementsByClassName("close-payment")[0].addEventListener("click", function() {
   document.getElementsByClassName("payment")[0].classList.add("opacity-0")
 })
+
+var quantity = document.getElementsByClassName("quantity")[0];
+quantity.addEventListener("change", function() {
+  document.getElementById("price").innerHTML = "$ " + quantity.value * 40;
+})
