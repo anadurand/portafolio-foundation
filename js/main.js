de5ba72a1bdab029a2b8ae1f5ab2398453004717
Mishmore <michellemore28@gmail.com>
2017-05-22 12:53:53 -0500

    payment

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ document.getElementsByClassName("open-acordeon")[0].addEventListener("click", fu
 document.getElementsByClassName("open-acordeon")[1].addEventListener("click", function() {
   document.getElementsByClassName("acordeon")[0].classList.remove("bottom-0");
 })
+
+document.getElementsByClassName("checkout")[0].addEventListener("click", function() {
+  document.getElementsByClassName("payment")[0].classList.remove("opacity-0")
+})
 /*
 document.getElementsByClassName("no-bullet")[0].addEventListener("click", function() {
   document.getElementsByClassName("acordeon")[0].classList.toggle("bottom-0");
